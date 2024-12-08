e1f7e884eaec17157f51cdd4a7214d19da207234
i-kamran <ibragimovkmrn@gmail.com>
2024-12-08 22:47:51 +0500

    feat: Add insert and grow methods in Array.java

--- src/Array.java
+++ src/Array.java
@@ -1,13 +1,24 @@
 public class Array {
     private int[] items;
+    public int count;
     public Array(int length){
-
         items = new int[length]
     }
     public void print(){
-        for (int i = 0 ; i < items.length; i++){
+        for (int i = 0 ; i < count; i++){
             System.out.println(items[i])
         }
+    }
+    public insert(int item){
+        if (count === items.length) grow()
+        items[count++] = item
+    }
+    private grow(){
+        int[] newItems = new int[count * 2]
+        for (let i = 0; i < count; ++i){
+            newItems[i] = items[i] 
+        }
+        items = newItems
     }
 }
 
