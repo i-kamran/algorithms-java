3384cbfa7e0dae39b7515d23067e4fcf3dd016cb
i-kamran <ibragimovkmrn@gmail.com>
2024-12-09 18:05:25 +0500

    feat: Add Main class

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * The Main class serves as the entry point for the application. It contains the main method where
+ * the program execution begins.
+ */
+public class Main {
+  /**
+   * The main method is the starting point of the Java application. It is responsible for
+   * initializing the program and orchestrating various operations.
+   *
+   * @param args command-line arguments passed to the program (if any)
+   */
+  public static void main(String[] args) {}
+}
