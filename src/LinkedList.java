88a47125bb9b0d889f6e06176f6d3f11ce39bf74
i-kamran <ibragimovkmrn@gmail.com>
2024-12-09 19:03:34 +0500

    feat: Add removeFirst and removeLast in LinkedList

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class LinkedList {
   private class Node {
     private int value;
@@ -36,16 +38,53 @@ public class LinkedList {
     }
   }
 
+  public int removeFirst() {
+    if (isEmpty()) throw new NoSuchElementException();
+    var value = this.head.value;
+    var second = this.head.next;
+    this.head.next = null;
+    this.head = second;
+    if (this.isEmpty()) {
+      this.tail = null;
+    }
+    return value;
+  }
+
+  public int removeLast() {
+    if (this.isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    var value = this.tail.value;
+    var prev = getPrevious(this.tail);
+    this.tail = prev;
+    this.tail.next = null;
+    return value;
+  }
+
+  private Node getPrevious(Node node) {
+    var curr = this.head;
+    while (curr != null) {
+      if (curr.next == node) {
+        return curr;
+      }
+      curr = curr.next;
+    }
+    return null;
+  }
+
   public int indexOf(int item) {
     var curr = this.head;
     for (var i = 0; curr != null; ++i) {
       if (curr.value == item) {
         return i;
       }
-
       curr = curr.next;
     }
     return -1;
+  }
+
+  public boolean contains(int item) {
+    return indexOf(item) != -1;
   }
 
   private boolean isEmpty() {
