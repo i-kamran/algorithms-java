7141c040134e7ff0d9f8562d7b9cf7f7781027ea
i-kamran <ibragimovkmrn@gmail.com>
2024-12-11 13:35:42 +0500

    feat: Add balanced bracket/parenthesis/brace checker

---
+++ src/Expressions.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Expressions {
+  private List<Character> opening = Arrays.asList('(', '[', '{', '<');
+  private List<Character> closing = Arrays.asList(')', ']', '}', '>');
+
+  public boolean isBalanced(String input) {
+    LinkedListStack<Character> stack = new LinkedListStack<>();
+    for (char ch : input.toCharArray()) {
+      if (isOpening(ch)) {
+        stack.push(ch);
+      }
+      if (isClosing(ch)) {
+        if (stack.isEmpty()) {
+          return false;
+        }
+        var top = stack.pop();
+        if (mapping(ch) != top) {
+          return false;
+        }
+      }
+    }
+    return stack.isEmpty();
+  }
+
+  private char mapping(char ch) {
+    switch (ch) {
+      case ')':
+        return '(';
+      case '}':
+        return '{';
+      case ']':
+        return '[';
+      case '>':
+        return '<';
+      default:
+        return ' ';
+    }
+  }
+
+  private boolean isOpening(Character ch) {
+    return opening.contains(ch);
+  }
+
+  private boolean isClosing(Character ch) {
+    return closing.contains(ch);
+  }
+}
