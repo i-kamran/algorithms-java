d214191d515e138b5616246c8b8e8b8c70518920
i-kamran <ibragimovkmrn@gmail.com>
2024-12-14 22:54:07 +0500

    feat: Add HashTable private HashEntry class

---
+++ src/HashTable.java
@@ -0,0 +1,11 @@
+public class HashTable {
+  private class HashEntry {
+    private int key;
+    private String value;
+
+    private HashEntry(int key, String value) {
+      this.key = key;
+      this.value = value;
+    }
+  }
+}
