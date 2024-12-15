9176f20acd9fe297e4494514d68fe1c03be12360
i-kamran <ibragimovkmrn@gmail.com>
2024-12-15 23:28:06 +0500

    feat: Add find method and fix scope

--- src/Tree.java
+++ src/Tree.java
@@ -7,31 +7,48 @@ public class Tree {
     private TreeNode(int value) {
       this.value = value;
     }
+  }
 
-    private TreeNode root;
+  private TreeNode root;
 
-    public void insert(int value) {
-      var newNode = new TreeNode(value);
-      if (this.root == null) {
-        this.root = newNode;
-        return;
-      }
-      var curr = root;
-      while (true) {
-        if (value < curr.value) {
-          if (curr.leftChild == null) {
-            curr.leftChild = newNode;
-            break;
-          }
-          curr = curr.leftChild;
-        } else {
-          if (curr.rightChild == null) {
-            curr.rightChild = newNode;
-            break;
-          }
-          curr = curr.rightChild;
+  public void insert(int value) {
+    var newNode = new TreeNode(value);
+    if (this.root == null) {
+      this.root = newNode;
+      return;
+    }
+    var curr = root;
+    while (true) {
+      if (value < curr.value) {
+        if (curr.leftChild == null) {
+          curr.leftChild = newNode;
+          break;
         }
+        curr = curr.leftChild;
+      } else {
+        if (curr.rightChild == null) {
+          curr.rightChild = newNode;
+          break;
+        }
+        curr = curr.rightChild;
       }
     }
+  }
+
+  public boolean find(int value) {
+    if (this.root == null) {
+      throw new IllegalStateException("Cannot search an empty tree.");
+    }
+    var curr = this.root;
+    while (curr != null) {
+      if (value == curr.value) {
+        return true;
+      } else if (value > curr.value) {
+        curr = curr.rightChild;
+      } else {
+        curr = curr.leftChild;
+      }
+    }
+    return false;
   }
 }
