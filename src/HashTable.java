88b741f6aa96c8e3c122dadcb551cd6c592b12e7
i-kamran <ibragimovkmrn@gmail.com>
2025-01-09 21:39:25 +0500

    feat: Add put and helper methods to HashTable class

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class HashTable {
   private class HashEntry {
     private int key;
@@ -7,5 +9,52 @@ public class HashTable {
       this.key = key;
       this.value = value;
     }
+  }
+
+  private LinkedList<HashEntry>[] table;
+
+  public HashTable() {
+    this(100);
+  }
+
+  public HashTable(int capacity) {
+    table = new LinkedList[capacity];
+  }
+
+  public void put(int key, String value) {
+    var entry = getEntry(key);
+    if (entry != null) {
+      entry.value = value;
+      return;
+    }
+    getOrCreateBucket(key).add(new HashEntry(key, value));
+  }
+
+  private LinkedList<HashEntry> getOrCreateBucket(int key) {
+    var bucket = getBucket(key);
+    if (bucket == null) {
+      table[hash(key)] = new LinkedList<>();
+    }
+    return bucket;
+  }
+
+  public HashEntry getEntry(int key) {
+    var bucket = getBucket(key);
+    if (bucket != null) {
+      for (var entry : bucket) {
+        if (entry.key == key) {
+          return entry;
+        }
+      }
+    }
+    return null;
+  }
+
+  private LinkedList<HashEntry> getBucket(int key) {
+    return table[hash(key)];
+  }
+
+  private int hash(int key) {
+    return key % table.length;
   }
 }
