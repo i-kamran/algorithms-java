b60f4db76d9e10a4efa0a9120f9f3c22b6350d4e
i-kamran <ibragimovkmrn@gmail.com>
2025-01-14 16:37:57 +0500

    feat: Add solution for twoSum

--- src/HashTableExercises.java
+++ src/HashTableExercises.java
@@ -1,4 +1,9 @@
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.print.attribute.standard.OutputBin;
 
 public class HashTableExercises {
   public static int mostFrequent(int[] items) {
@@ -16,6 +21,7 @@ public class HashTableExercises {
     for (var item : items) {
       int newFrequency = table.get(item);
       if (newFrequency > maxFrequency) {
+
         maxFrequency = newFrequency;
         mostFrequentItem = item;
       }
@@ -23,9 +29,23 @@ public class HashTableExercises {
     return mostFrequentItem;
   }
 
+  public static int[] twoSum(int[] input, int target) {
+    Set<Integer> set = new HashSet<>();
+    for (var item : input) {
+      if (set.contains(item)) {
+        return new int[] {item, target - item};
+      }
+      set.add(target - item);
+    }
+    return new int[] {-1};
+  }
+
   public static void main(String[] args) {
     int[] items = {1, 2, 2, 3, 3, 5, 5, 5};
     int result = mostFrequent(items);
     System.out.println("Most frequent item: " + result); // Output: Most frequent count: 3
+    int[] twoSumArr = {1, 2, 7, 11, 15};
+    int[] twoSumResult = twoSum(twoSumArr, 9);
+    System.out.println("Two sum answer:  " + Arrays.toString(twoSumResult) );
   }
 }
