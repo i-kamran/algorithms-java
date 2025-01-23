0108a0d057b6c9f9998e0d47d97289935e5f149b
i-kamran <ibragimovkmrn@gmail.com>
2025-01-23 23:16:35 +0500

    feat: Add getEntry method implementation

--- src/HashSet.java
+++ src/HashSet.java
@@ -22,9 +22,22 @@ public class HashSet<T> {
     bucket.add(value);
   }
 
-  private T getEntry(T value) {}
+  private T getEntry(T value) {
+    var bucket = getBucket(value);
+    if (bucket == null) {
+      return null;
+    }
+    for (var item : bucket) {
+      if (item == value) {
+        return value;
+      }
+    }
+    return null;
+  }
+
+  private LinkedListGeneric<T> getBucket(T value) {
+    return (items[hash(value)]);
+  }
 
   private LinkedListGeneric<T> getOrCreateBucket(T value) {}
-
-  private int hash(T value) {}
 }
