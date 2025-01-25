9a0189990254a466890e28c0bc7340464dce06c8
i-kamran <ibragimovkmrn@gmail.com>
2025-01-25 23:33:55 +0500

    feat(HeapMax): Add heapifyUp, getIndex methods

--- src/HeapMax.java
+++ src/HeapMax.java
@@ -28,7 +28,38 @@ public class HeapMax {
     items = newArr;
   }
 
-  private void heapifyUp() {}
+  private void heapifyUp(int idx) {
+    if (idx == 0) {
+      return;
+    }
+    int parentIdx = getParentIdx(idx);
+    if (items[idx] < items[parentIdx]) {
+      return;
+    }
+    swap(items, idx, parentIdx);
+    heapifyUp(parentIdx);
+  }
+
+  private void heapifyDown(int idx){
+  }
+
+  private void swap(int[] arr, int idxA, int idxB) {
+    int temp = arr[idxA];
+    arr[idxA] = arr[idxB];
+    arr[idxB] = temp;
+  }
+
+  private int getParentIdx(int idx) {
+    return (idx - 1) / 2;
+  }
+
+  private int getLeftChildIdx(int idx) {
+    return idx * 2 + 1;
+  }
+
+  private int getRightChildIdx(int idx) {
+    return idx * 2 + 2;
+  }
 
   @Override
   public String toString() {
