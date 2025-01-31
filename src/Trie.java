49f8ae7748abfdb63d0fb5cc25d4a4f007470368
i-kamran <ibragimovkmrn@gmail.com>
2025-01-31 11:34:34 +0500

    feat: Add TrieNode method to manipulate a node

--- src/Trie.java
+++ src/Trie.java
@@ -1,14 +1,33 @@
+import java.util.HashMap;
+
 public class Trie {
   public static int ALPHABET_SIZE = 26;
 
   private class TrieNode {
     private char value;
-    private TrieNode[] children;
+    private HashMap<Character, TrieNode> children;
     private boolean isEndOfWord;
 
     private TrieNode(char value) {
       this.value = value;
-      children = new TrieNode[ALPHABET_SIZE];
+      children = new HashMap<>();
+    }
+
+    @Override
+    public String toString() {
+      return "value=" + value;
+    }
+
+    private boolean hasChild(char ch) {
+      return children.containsKey(ch);
+    }
+
+    private void addChild(char ch) {
+      children.put(ch, new TrieNode(ch));
+    }
+
+    private TrieNode getChild(char ch) {
+      return children.get(ch);
     }
   }
 
@@ -17,11 +36,10 @@ public class Trie {
   public void insert(String word) {
     var current = root;
     for (char ch : word.toCharArray()) {
-      int idx = ch - 'a';
-      if (current.children[idx] == null) {
-        current.children[idx] = new TrieNode(ch);
+      if (!current.hasChild(ch)) {
+        current.addChild(ch);
       }
-      current = current.children[idx];
+      current = current.getChild(ch);
     }
     current.isEndOfWord = true;
   }
