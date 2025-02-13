059174d094cc9afa94b08fa901c031b5d3cd1aae
i-kamran <ibragimovkmrn@gmail.com>
2025-02-13 20:34:57 +0500

    style: Improve formatting to comply with style guides

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,11 +1,11 @@
-public class SelectionSort{
-  public void sort(int[] arr){
-    for (int i = 0; i < arr.length; ++i){
+public class SelectionSort {
+  public void sort(int[] arr) {
+    for (int i = 0; i < arr.length; ++i) {
       int minIdx = i;
-      for (int j = i + 1; j < arr.length; ++j){
-        if (arr[j] < arr[minIdx]){
+      for (int j = i + 1; j < arr.length; ++j) {
+        if (arr[j] < arr[minIdx]) {
           minIdx = j;
-        } 
+        }
       }
       int temp = arr[i];
       arr[i] = arr[minIdx];
