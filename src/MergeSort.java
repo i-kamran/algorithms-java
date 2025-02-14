9d06ba1c8a23de034750dd383b3470f56602043f
i-kamran <ibragimovkmrn@gmail.com>
2025-02-14 21:44:30 +0500

    feat(MergeSort): Add merge method

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -14,4 +14,32 @@ public class MergeSort {
     sort(arr, mid + 1, right);
     merge(arr, left, mid, right);
   }
+
+  private void merge(int[] arr, int left, int mid, int right) {
+    int leftSize = mid - left + 1;
+    int rightSize = right - mid;
+    int[] leftArr = new int[leftSize];
+    int[] rightArr = new int[rightSize];
+    copyArray(arr, leftArr, left);
+    copyArray(arr, rightArr, mid + 1);
+
+    int leftPointer = 0;
+    int rightPointer = 0;
+    int pointer = left;
+
+    while (leftPointer < leftSize && rightPointer < rightSize) {
+      if (leftArr[leftPointer] < rightArr[rightPointer]) {
+        arr[pointer++] = leftArr[leftPointer++];
+      } else {
+        arr[pointer++] = rightArr[rightPointer++];
+      }
+    }
+
+    while (leftPointer < leftSize) {
+      arr[pointer++] = leftArr[leftPointer++];
+    }
+    while (rightPointer < rightSize) {
+      arr[pointer++] = rightArr[rightPointer++];
+    }
+  }
 }
