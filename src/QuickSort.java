4748e351f775ced7e9caaeb47a6ff3a6a5b745d2
i-kamran <ibragimovkmrn@gmail.com>
2025-02-15 22:50:32 +0500

    feat: Add partition private method in QuickSort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class QuickSort {
   public void sort(int[] arr) {
     quickSort(arr, 0, arr.length - 1);
@@ -7,17 +9,35 @@ public class QuickSort {
     if (left >= right) {
       return;
     }
-    int partitionIdx = partition(arr, left, right);
-    quickSort(arr, left, partitionIdx);
-    quickSort(arr, partitionIdx + 1, right);
+    int pivotIdx = partition(arr, left, right);
+    quickSort(arr, left, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, right);
   }
 
   public int partition(int[] arr, int left, int right) {
-
+    int pivotIdx = (right - left) / 2 + left;
+    swap(arr, pivotIdx, right);
+    int pointer = left;
+    for (int i = left; i < right; ++i) {
+      if (arr[i] < arr[right]) {
+        swap(arr, i, pointer++);
+      }
+    }
+    swap(arr, pointer, right);
+    return pointer;
   }
-  public void swap(int[] arr, int aIdx, int bIdx){
-    int temp = arr[aIdx];
-    arr[aIdx] = arr[bIdx];
-    arr[bIdx] = temp;
-  } 
+
+  public void swap(int[] arr, int idxA, int idxB) {
+    int temp = arr[idxA];
+    arr[idxA] = arr[idxB];
+    arr[idxB] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {5, 1, 1, 2, 0, 0};
+    System.out.println(Arrays.toString(arr));
+    MergeSort sorter = new MergeSort();
+    sorter.sort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
 }
