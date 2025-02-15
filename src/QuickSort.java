37f6c46d8bb7404dc46a60dc33d7b0f69a1b0e3c
i-kamran <ibragimovkmrn@gmail.com>
2025-02-15 22:53:15 +0500

    style: Add braces to comply with linter rules

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -93,7 +93,9 @@ public class QuickSort {
 
   private static boolean isSorted(int[] arr) {
     for (int i = 1; i < arr.length; i++) {
-      if (arr[i] < arr[i - 1]) return false;
+      if (arr[i] < arr[i - 1]) {
+        return false;
+      }
     }
     return true;
   }
