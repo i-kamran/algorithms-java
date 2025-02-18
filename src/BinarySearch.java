b6922da420297f4258c5bb1aad21e112dc38c1ff
i-kamran <ibragimovkmrn@gmail.com>
2025-02-18 23:53:10 +0500

    feat: Add recursive binary search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -7,7 +7,6 @@ public class BinarySearch {
     int right = arr.length - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      System.out.println(mid);
       if (needle == arr[mid]) {
         return true;
       }
@@ -20,9 +19,27 @@ public class BinarySearch {
     return false;
   }
 
+  public boolean searchRecursive(int[] arr, int needle) {
+    return searchWalk(arr, needle, 0, arr.length - 1);
+  }
+
+  private boolean searchWalk(int[] arr, int needle, int left, int right) {
+    if (left > right) {
+      return false;
+    }
+    int mid = left + (right - left) / 2;
+    if (needle == arr[mid]) {
+      return true;
+    }
+    if (needle < arr[mid]) {
+      return searchWalk(arr, needle, left, mid - 1);
+    } else {
+      return searchWalk(arr, needle, mid + 1, right);
+    }
+  }
+
   public static void main(String[] args) {
     BinarySearch searcher = new BinarySearch();
     int[] arr = {1, 4, 7, 9, 11, 15};
-    System.out.println(searcher.search(arr, 5));
   }
 }
