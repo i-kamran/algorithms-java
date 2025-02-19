69353d48061ad81e11f9300bee57ca27b133fa81
i-kamran <ibragimovkmrn@gmail.com>
2025-02-19 23:36:05 +0500

    feat: Add recursive ternary search

--- src/TernarySearch.java
+++ src/TernarySearch.java
@@ -23,4 +23,27 @@ public class TernarySearch {
     }
     return false;
   }
+
+  public boolean searchRecursive(int[] arr, int needle) {
+    return searchRecursiveWalk(arr, needle, 0, arr.length - 1);
+  }
+
+  private boolean searchRecursiveWalk(int[] arr, int needle, int left, int right) {
+    if (left > right) {
+      return false;
+    }
+    int partitionSize = (right - left) / 3;
+    int mid1 = left + partitionSize;
+    int mid2 = right - partitionSize;
+    if (needle == arr[mid1] || needle == arr[mid2]) {
+      return true;
+    }
+    if (needle < arr[mid1]) {
+      return searchRecursiveWalk(arr, needle, left, mid1 - 1);
+    }
+    if (needle > arr[mid1] && needle < arr[mid2]) {
+      return searchRecursiveWalk(arr, needle, mid1 + 1, mid2 - 1);
+    }
+    return searchRecursiveWalk(arr, needle, mid2 + 1, right);
+  }
 }
