dcaf9e0d3b281dba54d69cf8c09c0d638ae3ad05
i-kamran <ibragimovkmrn@gmail.com>
2025-02-20 23:26:37 +0500

    feat(StringUtils): Add method to reverse a string

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -1,21 +1,29 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class StringUtils{
-  public static int countVowels(String str){
+public class StringUtils {
+  public static int countVowels(String str) {
     if (str == null) {
       return 0;
     }
     int count = 0;
     Set<Character> vowels = new HashSet<>();
-      for (char ch: "aeiou".toCharArray()){
+    for (char ch : "aeiou".toCharArray()) {
       vowels.add(ch);
     }
-    for (var ch: str.toLowerCase().toCharArray()){
-      if (vowels.contains(ch)){
+    for (var ch : str.toLowerCase().toCharArray()) {
+      if (vowels.contains(ch)) {
         count++;
       }
     }
     return count;
+  }
+
+  public static String reverse(String str) {
+    StringBuilder reversed = new StringBuilder();
+    for (int i = str.length() - 1; i >= 0; --i) {
+      reversed.append(str.charAt(i));
+    }
+    return reversed.toString();
   }
 }
