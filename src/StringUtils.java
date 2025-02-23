f3b74f4eb35f2adc79216cd6c97fef56c4f1dd69
i-kamran <ibragimovkmrn@gmail.com>
2025-02-23 22:15:05 +0500

    feat(StringUtils): Add isPalindrome method

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -113,4 +113,16 @@ public class StringUtils {
     }
     return true;
   }
+
+  public static boolean isPalindrome(String word) {
+    if (word == null) {
+      return false;
+    }
+    for (int i = 0; i < word.length(); ++i) {
+      if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
